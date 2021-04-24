215272b573c0a8864f03e27cc1f2e3dab9de4b11
sonia <atresonia@gmail.com>
2021-04-24 14:55:27 -0700

    Fixed buttons

--- scenes/EndScene.js
+++ scenes/EndScene.js
@@ -1,45 +1,40 @@
 class EndScene extends Phaser.Scene {
     constructor() {
-        super({key: 'endScene'});
+        super({ key: 'endScene' });
     }
 
     preload() {
         this.load.image('endScene', 'assets/endScene.png');
+        this.load.image('restartButton', 'assets/restartButton.png');
+        this.load.image('quitButton', 'assets/quitButton2.png');
     }
 
     create() {
         //title
-        var img = this.add.image(game.config.width, 
+        var img = this.add.image(game.config.width,
             game.config.height, 'endScene');
         img.scale = 0.18
         img.setOrigin(1.5, 1.3);
+        var highscore = this.add.text('High score: ');
 
-        //button play
-        this.buttonPlay = this.add.text(game.config.width/2 - 25,
-            game.config.height/2 + 40, 'RESTART',
-        {fill: '#FFFFFF'});
-        this.buttonPlay.setInteractive();
-        this.buttonPlay.on('pointerdown', () => { 
+        //button restart
+        var restartButton = this.add.image(game.config.width,
+            game.config.height, 'restartButton');
+        restartButton.scale = 0.18
+        restartButton.setOrigin(2.1, 5);
+        restartButton.setInteractive();
+        restartButton.on('pointerdown', () => {
             this.clickButton();
         });
-
-        //button quit
-        this.buttonQuit = this.add.text(game.config.width/2 - 12,
-            game.config.height/1.35 - 5, 'QUIT',
-        {fill: '#FFFFFF'});
-        this.buttonQuit.setInteractive();
-        this.buttonQuit.on('pointerdown', () => { 
+        // button quit
+        var quitButton = this.add.image(game.config.width,
+            game.config.height, 'quitButton');
+        quitButton.scale = 0.18
+        quitButton.setOrigin(2.1, 3.5);
+        quitButton.setInteractive();
+        quitButton.on('pointerdown', () => {
             this.clickButton();
         });
-
-        //button
-        // this.buttonPlay = this.add.text(game.config.width/2,
-        //     game.config.height/2, 'PLAY',
-        // {fill: '#00FF00'});
-        // this.buttonPlay.setInteractive();
-        // this.buttonPlay.on('pointerdown', () => { 
-        //     console.log('pointerdown'); 
-        // });
     }
 
     clickButton() {
--- scenes/TitleScene.js
+++ scenes/TitleScene.js
@@ -1,34 +1,28 @@
 class TitleScene extends Phaser.Scene {
     constructor() {
-        super({key: 'titleScene'});
+        super({ key: 'titleScene' });
     }
 
     preload() {
         this.load.image('titleScreen', 'assets/Title.png');
+        this.load.image('playButton', 'assets/playButton.png');
     }
 
     create() {
+
+
         //title
-        var img = this.add.image(game.config.width, 
+        var title = this.add.image(game.config.width,
             game.config.height, 'titleScreen');
-        img.scale = 0.5
-        img.setOrigin(1.25, 1.4);
+        title.scale = 0.5
+        title.setOrigin(1.25, 1.4);
 
-        //button play
-        this.buttonPlay = this.add.text(game.config.width/2 - 5,
-            game.config.height/2 - 3, 'PLAY',
-        {fill: '#FFFFFF'});
-        this.buttonPlay.setInteractive();
-        this.buttonPlay.on('pointerdown', () => { 
-            this.clickButton();
-        });
-
-        //button quit
-        this.buttonQuit = this.add.text(game.config.width/2 - 5,
-            game.config.height/1.4 - 10, 'QUIT',
-        {fill: '#FFFFFF'});
-        this.buttonQuit.setInteractive();
-        this.buttonQuit.on('pointerdown', () => { 
+        var playButton = this.add.image(game.config.width,
+            game.config.height, 'playButton');
+        playButton.scale = 0.5
+        playButton.setOrigin(1.9, 5);
+        playButton.setInteractive();
+        playButton.on('pointerdown', () => {
             this.clickButton();
         });
     }
